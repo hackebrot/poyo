a64b5896b6bd8da36b3e2d2d5bd8b48c4c355935
Raphael Pierzina <raphael@hackebrot.de>
2015-12-27 22:23:29 +0100

    Create an init file with meta information

---
+++ poyo/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Raphael Pierzina'
+__email__ = 'raphael@hackebrot.de'
+__version__ = '0.1.0'
+
+from .parser import parse_string
+
+__all__ = ['parse_string']
