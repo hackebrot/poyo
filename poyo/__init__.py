faae5df8648afbfa5921bd67a7f3e082ba626a95
Raphael Pierzina <raphael@hackebrot.de>
2016-02-15 18:23:04 +0000

    Move module level import to top of file

--- poyo/__init__.py
+++ poyo/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from .parser import parse_string
+
 __author__ = 'Raphael Pierzina'
 __email__ = 'raphael@hackebrot.de'
 __version__ = '0.1.0'
-
-from .parser import parse_string
 
 __all__ = ['parse_string']
