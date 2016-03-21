a7a1bcfb2e611c434e93a06679b18d173d290920
Raphael Pierzina <raphael@hackebrot.de>
2016-03-21 01:15:41 +0000

    Add word boundaries to regex patterns

--- poyo/patterns.py
+++ poyo/patterns.py
@@ -13,9 +13,9 @@ BLANK_LINE = r"^[ \t]*" + NEWLINE
 SECTION = INDENT + VARIABLE + INLINE_COMMENT + NEWLINE
 SIMPLE = INDENT + VARIABLE + BLANK + VALUE + INLINE_COMMENT + NEWLINE
 
-NULL = r"null|Null|NULL|~"
-TRUE = r"true|True|TRUE"
-FALSE = r"false|False|FALSE"
+NULL = r"\b(null|Null|NULL|~)\b"
+TRUE = r"\b(true|True|TRUE)\b"
+FALSE = r"\b(false|False|FALSE)\b"
 INT = r"[-+]?[0-9]+"
 FLOAT = r"([-+]?(\.[0-9]+|[0-9]+(\.[0-9]*)?)([eE][-+]?[0-9]+)?)"
 STR = r"(?P<quotes>['\"]?).*(?P=quotes)"
