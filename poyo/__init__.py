8334bdf134441bad8d97f752e7f1f6d6fbfc42b1
Raphael Pierzina <raphael@hackebrot.de>
2016-04-13 19:40:40 +0100

    Expose PoyoException for package

--- poyo/__init__.py
+++ poyo/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from .exceptions import PoyoException
 from .parser import parse_string
 
 __author__ = 'Raphael Pierzina'
 __email__ = 'raphael@hackebrot.de'
 __version__ = '0.1.0'
 
-__all__ = ['parse_string']
+__all__ = ['parse_string', 'PoyoException']
