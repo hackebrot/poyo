c624aabe686997adf3168746796a862c971b3873
eykd <david.eyk@gmail.com>
2016-04-22 15:49:12 -0700

    Refactor patterns; add tests, better blanks support
    I apologize that this is all in one commit. I got going and the next thing I knew, I had gone too far without a commit.
    
    Some highlights:
    
    - **Compile patterns in `poyo.patterns` instead of `_Parser.__init__`.** This is important contextual information that belongs with the patterns themselves.
    - **Add tests for compiled patterns.**
    - **Add better blank line support.** The original impetus! Previously, blank lines in certain places, such as immediately after a section start or inside a list, would confuse the parser and raise an error. I've added support for blank lines in these situations.
    - **Moved test YAML for parser test inside of test module.** It wasn't really necessary to have since the parser only works with strings anyway, and it seemed easier to work on the test with everything in one place.
    
    The blank line support probably isn't perfect yet, and the new `LIST` pattern has grown rather hideous, but the tests pass and I think it's a good start.

--- poyo/patterns.py
+++ poyo/patterns.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import re
+
 INDENT = r"(?P<indent>^ *)"
 VARIABLE = r"(?P<variable>.+):"
 VALUE = r"(?P<value>(?:(?P<q2>['\"]).*?(?P=q2))|[^#]+?)"
@@ -15,7 +17,20 @@ SECTION = INDENT + VARIABLE + INLINE_COMMENT + NEWLINE
 SIMPLE = INDENT + VARIABLE + BLANK + VALUE + INLINE_COMMENT + NEWLINE
 
 LISTITEM = BLANK + r"-" + BLANK + VALUE + INLINE_COMMENT + NEWLINE
-LIST = SECTION + r"(?P<items>(?:(?P=indent)" + LISTITEM + r")+)"
+SIMPLE_LISTITEM = BLANK + r'-' + '.+' + NEWLINE
+LIST = (SECTION +
+        r"(?P<items>" + (
+            "(?:" + (
+                BLANK_LINE +
+                '|' + '#.*' + NEWLINE +
+                r')?') +
+            "(?:" + "(?P=indent)" + SIMPLE_LISTITEM + ")" +
+            "(?:" + (
+                BLANK_LINE +
+                '|' + '#.*' + NEWLINE +
+                '|' + "(?P=indent)" + SIMPLE_LISTITEM
+            ) + r")*)"
+        ))
 
 NULL = r"\b(null|Null|NULL|~)\b"
 TRUE = r"\b(true|True|TRUE)\b"
@@ -23,3 +38,19 @@ FALSE = r"\b(false|False|FALSE)\b"
 INT = r"[-+]?[0-9]+"
 FLOAT = r"([-+]?(\.[0-9]+|[0-9]+(\.[0-9]*)?)([eE][-+]?[0-9]+)?)"
 STR = r"(?P<quotes>['\"]?).*(?P=quotes)"
+
+LISTITEM_CP = re.compile(LISTITEM, re.MULTILINE)
+
+COMMENT_CP = re.compile(COMMENT, re.MULTILINE)
+BLANK_LINE_CP = re.compile(BLANK_LINE, re.MULTILINE)
+DASHES_CP = re.compile(DASHES, re.MULTILINE)
+LIST_CP = re.compile(LIST, re.MULTILINE)
+SIMPLE_CP = re.compile(SIMPLE, re.MULTILINE)
+SECTION_CP = re.compile(SECTION, re.MULTILINE)
+
+NULL_CP = re.compile(NULL)
+TRUE_CP = re.compile(TRUE)
+FALSE_CP = re.compile(FALSE)
+FLOAT_CP = re.compile(FLOAT)
+INT_CP = re.compile(INT)
+STR_CP = re.compile(STR)
---
+++ tests/test_patterns.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+from poyo import patterns
+
+
+def test_COMMENT_matches_a_comment_line():
+    match = patterns.COMMENT_CP.match('# baz\n - bar\nfoobar',)
+    assert match is not None
+    assert match.group() == '# baz\n'
+    assert match.groupdict() == {}
+
+
+def test_COMMENT_matches_a_comment_line_with_leading_whitespace():
+    match = patterns.COMMENT_CP.match('    # baz\n - bar\nfoobar',)
+    assert match is not None
+    assert match.group() == '    # baz\n'
+    assert match.groupdict() == {}
+
+
+def test_BLANK_LINE_matches_a_blank_line():
+    match = patterns.BLANK_LINE_CP.match('  \nfoobar')
+    assert match is not None
+    assert match.group() == '  \n'
+
+
+def test_DASHES_matches_document_separator():
+    match = patterns.DASHES_CP.match('---\nfoobar')
+    assert match is not None
+    assert match.group() == '---\n'
+
+
+def test_LIST_matches_a_list():
+    match = patterns.LIST_CP.match('hmm:\n  - foo\n - bar\n    - blah\n-bing')
+    assert match is not None
+    assert match.group() == 'hmm:\n  - foo\n - bar\n    - blah\n'
+    assert match.groupdict() == {
+        'variable': 'hmm',
+        'items': '  - foo\n - bar\n    - blah\n',
+        'indent': '',
+    }
+
+
+def test_LIST_matches_a_list_with_blank_lines():
+    match = patterns.LIST_CP.match('hmm:\n  - foo\n\n    - bar\n    - blah\n-bing')
+    assert match is not None
+    assert match.group() == 'hmm:\n  - foo\n\n    - bar\n    - blah\n'
+    assert match.groupdict() == {
+        'variable': 'hmm',
+        'items': '  - foo\n\n    - bar\n    - blah\n',
+        'indent': '',
+    }
+
+
+def test_LISTITEM_finds_all_listitems():
+    found = patterns.LISTITEM_CP.findall(' - foo # baz\n - bar\nfoobar',)
+    expected = [
+        ('foo', ''),
+        ('bar', ''),
+    ]
+    assert found is not None
+    assert found == expected
+
+
+def test_LISTITEM_finds_all_listitems_with_blank_lines():
+    found = patterns.LISTITEM_CP.findall(' - foo # baz\n\n - bar\nfoobar',)
+    expected = [
+        ('foo', ''),
+        ('bar', ''),
+    ]
+    assert found is not None
+    assert found == expected
+
+
+def test_SIMPLE_matches_a_simple_key_value_line():
+    match = patterns.SIMPLE_CP.match('   foo: bar # baz\n - bar\nfoobar',)
+    assert match is not None
+    assert match.group() == '   foo: bar # baz\n'
+    assert match.groupdict() == {
+        'indent': '   ',
+        'q2': None,
+        'value': 'bar',
+        'variable': 'foo',
+    }
+
+
+def test_SECTION_matches_a_section_key_line():
+    match = patterns.SECTION_CP.match('   foo: # baz\n      - bar\nfoobar',)
+    assert match is not None
+    assert match.group() == '   foo: # baz\n'
+    assert match.groupdict() == {
+        'indent': '   ',
+        'variable': 'foo',
+    }
+
+
+def test_NULL_matches_a_null_value():
+    for value in ('null', 'Null', 'NULL'):
+        match = patterns.NULL_CP.match(value + '   ')
+        assert match is not None
+        assert match.group() == value
+        assert match.groupdict() == {}
+
+
+def test_TRUE_matches_a_true_value():
+    for value in ('true', 'True', 'TRUE'):
+        match = patterns.TRUE_CP.match(value + '   ')
+        assert match is not None
+        assert match.group() == value
+        assert match.groupdict() == {}
+
+
+def test_FALSE_matches_a_false_value():
+    for value in ('false', 'False', 'FALSE'):
+        match = patterns.FALSE_CP.match(value + '   ')
+        assert match is not None
+        assert match.group() == value
+        assert match.groupdict() == {}
+
+
+def test_FLOAT_matches_a_float_value():
+    for value in ('1.0', '42.820308', '39.2234e9'):
+        match = patterns.FLOAT_CP.match(value + '   ')
+        assert match is not None
+        assert match.group() == value
+        assert match.groupdict() == {}
+
+
+def test_INT_matches_an_int_value():
+    for value in ('1', '42', '3922349'):
+        match = patterns.INT_CP.match(value + '   ')
+        assert match is not None
+        assert match.group() == value
+        assert match.groupdict() == {}
+
+
+def test_STR_matches_a_string_value():
+    value ='foo bar baz'
+    match = patterns.STR_CP.match(value + '   ')
+    assert match is not None
+    assert match.group() == value + '   '
+    assert match.groupdict() == {
+        'quotes': '',
+    }
+
+
+def test_STR_matches_a_single_quoted_string_value():
+    value ="'foo bar baz'"
+    match = patterns.STR_CP.match(value + '   ')
+    assert match is not None
+    assert match.group() == value
+    assert match.groupdict() == {
+        'quotes': "'",
+    }
+
+
+def test_STR_matches_a_double_quoted_string_value():
+    value ='"foo bar baz"'
+    match = patterns.STR_CP.match(value + '   ')
+    assert match is not None
+    assert match.group() == value
+    assert match.groupdict() == {
+        'quotes': '"',
+    }
