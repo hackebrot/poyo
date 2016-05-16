bc1e2d2f71fa381dd42ad414407f05b9e9e73e51
Raphael Pierzina <raphael@hackebrot.de>
2016-05-16 20:47:20 +0100

    Bump version: 0.2.0 → 0.3.0

--- poyo/__init__.py
+++ poyo/__init__.py
@@ -5,6 +5,6 @@ from .parser import parse_string
 
 __author__ = 'Raphael Pierzina'
 __email__ = 'raphael@hackebrot.de'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 __all__ = ['parse_string', 'PoyoException']
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name='poyo',
-    version='0.2.0',
+    version='0.3.0',
     author='Raphael Pierzina',
     author_email='raphael@hackebrot.de',
     maintainer='Raphael Pierzina',
