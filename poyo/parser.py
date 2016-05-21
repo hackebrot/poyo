1197eae3661ee2b25ecc340ffdbcf9b9fb7907af
Raphael Pierzina <raphael@hackebrot.de>
2016-05-21 17:20:23 +0100

    Add debug logging to parser callbacks

--- poyo/parser.py
+++ poyo/parser.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+
+import functools
+import logging
+
 from ._nodes import Root, Section, Simple
 from .exceptions import NoMatchException, NoParentException, NoTypeException
 
@@ -6,6 +10,16 @@ from .patterns import (
     COMMENT, BLANK_LINE, DASHES, LIST, SIMPLE, SECTION,
     LIST_ITEM, NULL, TRUE, FALSE, FLOAT, INT, STR,
 )
+
+logger = logging.getLogger(__name__)
+
+
+def log_callback(wrapped_function):
+    @functools.wraps(wrapped_function)
+    def _wrapper(parser, match, **kwargs):
+        logger.debug('{}:{}'.format(wrapped_function.__name__, match))
+        return wrapped_function(parser, match, **kwargs)
+    return _wrapper
 
 
 class _Parser(object):
@@ -58,15 +72,19 @@ class _Parser(object):
             'Unable to determine type for "{}"'.format(string)
         )
 
+    @log_callback
     def parse_comment(self, match):
         pass
 
+    @log_callback
     def parse_blankline(self, match):
         pass
 
+    @log_callback
     def parse_dashes(self, match):
         pass
 
+    @log_callback
     def parse_list(self, match):
         groups = match.groupdict()
         level = len(groups['indent'])
@@ -87,6 +105,7 @@ class _Parser(object):
         )
         self.seen.append(simple)
 
+    @log_callback
     def parse_simple(self, match):
         groups = match.groupdict()
 
@@ -101,6 +120,7 @@ class _Parser(object):
         )
         self.seen.append(simple)
 
+    @log_callback
     def parse_section(self, match):
         groups = match.groupdict()
 
@@ -114,21 +134,27 @@ class _Parser(object):
         )
         self.seen.append(section)
 
+    @log_callback
     def parse_null(self, match):
         return None
 
+    @log_callback
     def parse_true(self, match):
         return True
 
+    @log_callback
     def parse_false(self, match):
         return False
 
+    @log_callback
     def parse_int(self, match):
         return int(match.group())
 
+    @log_callback
     def parse_float(self, match):
         return float(match.group())
 
+    @log_callback
     def parse_str(self, match):
         quotes = match.group('quotes')
         return match.group().strip(quotes)
