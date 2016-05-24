565c95ce9a8ff96d177196c6dbf8d8f88cdfa029
Raphael Pierzina <raphael@hackebrot.de>
2016-05-24 23:28:37 +0100

    Add an error class for string data that is ignored by the parser

--- poyo/exceptions.py
+++ poyo/exceptions.py
@@ -20,3 +20,9 @@ class NoTypeException(PoyoException):
     """Raised when the parser is unable to determine the actual type for a
     given string.
     """
+
+
+class IgnoredMatchException(PoyoException):
+    """Raised when a match does result in a Python representation such as a
+    comment or a blank line.
+    """
