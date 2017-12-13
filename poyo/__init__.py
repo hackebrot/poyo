d7871ea195ca1d1f25e2d4e371fe32bf59c17fcb
Rajiv Shivane <rajiv.shivane@agilx.ai>
2017-12-13 16:51:11 +0530

    Fixes for Python 2.6 compatibility

--- poyo/__init__.py
+++ poyo/__init__.py
@@ -5,10 +5,19 @@ import logging
 from .exceptions import PoyoException
 from .parser import parse_string
 
+try:
+    from logging import NullHandler
+except ImportError:
+    from logging import Handler
+
+    class NullHandler(Handler):
+        def emit(self, record):
+            pass
+
 __author__ = 'Raphael Pierzina'
 __email__ = 'raphael@hackebrot.de'
 __version__ = '0.4.1'
 
-logging.getLogger(__name__).addHandler(logging.NullHandler())
+logging.getLogger(__name__).addHandler(NullHandler())
 
 __all__ = ['parse_string', 'PoyoException']
