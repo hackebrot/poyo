8a127bba9851dd1ff9f59e4140e7c2699a647ae4
Manuel Ebert <manuel@1450.me>
2019-05-03 08:19:46 -0700

    Bump version: 0.4.2 → 0.5.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name='poyo',
-    version='0.4.2',
+    version='0.5.0',
     author='Raphael Pierzina',
     author_email='raphael@hackebrot.de',
     maintainer='Raphael Pierzina',
